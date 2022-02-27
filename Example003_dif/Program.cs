3517a34a4f04e67e92fe108e38bc91d78f034e53
Illyashevich Oleg <vechy_oleg@mail.ru>
2022-02-27 21:36:16 +0300

    initial commit

---
+++ Example003_dif/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example003_dif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
