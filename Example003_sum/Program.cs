559f7d7e2da2fdf5ae3eccf1a881bd3b8fa15021
Illyashevich Oleg <vechy_oleg@mail.ru>
2022-02-27 21:21:18 +0300

    add Example003_sum

---
+++ Example003_sum/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Example003_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberA = 3; 
+            int numberB = 33;
+            int result = numberA + numberB;
+            // Console.WriteLine(numberA + numberB);
+            Console.WriteLine(result);
+        }
+    }
+}
