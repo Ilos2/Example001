449a5a7627683c2d9df0a00832af810d4cfe365a
Illyashevich Oleg <vechy_oleg@mail.ru>
2022-03-04 22:57:22 +0300

    Example005_ConditinIFEL

---
+++ Example005_ConditinIFEL/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Example005_ConditinIFEL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя пользователя");
+            string username = Console.ReadLine();
+
+            if (username.ToLower() == "маша")
+
+            {
+                Console.WriteLine("Ура, это же Маша!");
+            }
+            
+            else
+            
+            {
+                Console.Write("Привет, ");
+                Console.WriteLine(username);
+            }
+        }
+    }
+}
