d71241fa20e053973934a8b3e22b490cd8ad16d9
Illyashevich Oleg <vechy_oleg@mail.ru>
2022-03-07 20:52:43 +0300

    add base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Example006_Base
+{
+    class Program
+    {
+        static  void Main(string[] args)
+        {
+            
+            int a = 1;
+            int b = 7;
+            int c = 5;
+            int d = 9;
+
+            int max = a;
+            {
+                if (a > max) max = a;
+                if (b > max) max = b;
+                if (c > max) max = c;
+                if (d > max) max = d;
+            }
+
+            Console.Write ("max= ");
+            Console.WriteLine(max);
+        }
+    }
+}
